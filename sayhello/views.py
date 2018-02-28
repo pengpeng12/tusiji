e03a0b94c6210cbaeb4431ec5bc227f1c8a29690
songpengpeng <songpengpeng@localhost.localdomain>
2018-02-28 14:39:45 +0800

    update

--- sayhello/views.py
+++ sayhello/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def sayhello(request):
-    return HttpResponse('hello,tusiji0123456789');
+    return HttpResponse('hello,tusiji   0123456789');
 
